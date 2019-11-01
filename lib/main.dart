09aa8d6c3f20be48380bfab325907d90af84cdcc
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2019-11-01 11:36:01 +0100

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your Daily Tasks',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Daily Tasks'),
+    );
+  }
+}
+
+class DailyTask {
+  DailyTask({
+    this.title,
+    this.counter,
+  });
+
+  String title;
+  int counter;
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<DailyTask> _dailyTasks = <DailyTask>[
+    DailyTask(title: 'Title 0', counter: 0),
+    DailyTask(title: 'Title 1', counter: 0),
+    DailyTask(title: 'Title 2', counter: 0),
+  ];
+
+  void _addDailyTask() {
+    setState(
+      () {
+        int currentIndex = _dailyTasks.length;
+        _dailyTasks.add(
+          DailyTask(title: 'Title $currentIndex', counter: 0),
+        );
+      },
+    );
+  }
+
+  List<Widget> _getCells(List<DailyTask> tasks) {
+    return tasks.map(
+      (DailyTask currentTask) {
+        return Card(
+          child: ListTile(
+            title: Text(currentTask.title),
+            trailing: IconButton(
+              icon: Icon(Icons.more_vert),
+              tooltip: 'Edit',
+              onPressed: () {
+                setState(
+                  () {},
+                );
+              },
+            ),
+          ),
+        );
+      },
+    ).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: _getCells(_dailyTasks),
+        /*children: const <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('One-line ListTile'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(),
+              title: Text('One-line with leading widget'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('One-line with trailing widget'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(),
+              title: Text('One-line with both widgets'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('One-line dense ListTile'),
+              dense: true,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(size: 56.0),
+              title: Text('Two-line ListTile'),
+              subtitle: Text('Here is a second line'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(size: 72.0),
+              title: Text('Three-line ListTile'),
+              subtitle: Text(
+                'A sufficiently long subtitle warrants three lines.',
+              ),
+              trailing: Icon(Icons.more_vert),
+              isThreeLine: true,
+            ),
+          ),
+        ],*/
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addDailyTask,
+        tooltip: 'Add Daily Task',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
