371bdedb7c8f6fb5fb648e07dfc5ff7720f2a45b
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2020-03-31 08:25:43 +0200

    Move popupmenu to own file

---
+++ lib/popup_icon_menu.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:popup_menu/popup_menu.dart';
+
+import 'package:daily_task_app/daily_task.dart';
+import 'package:daily_task_app/data_store.dart';
+import 'package:daily_task_app/task_screen.dart';
+
+void openIconMenu(List<String> iconStrings, GlobalKey menuKey) {
+  List<MenuItem> items = iconStrings.map(
+    (String currentIconString) {
+      return MenuItem(
+        title: currentIconString,
+        image: Icon(
+          MdiIcons.fromString(currentIconString),
+          color: Colors.white,
+        ),
+      );
+    },
+  ).toList();
+
+  /*List<MenuItem> items = <vMenuItem>[
+      MenuItem(
+        title: 'Copy',
+        image: Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+      ),
+      MenuItem(
+        title: 'Home',
+        textStyle: TextStyle(fontSize: 10.0, color: Colors.tealAccent),
+        image: Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+      ),
+      MenuItem(
+        title: 'Mail',
+        image: Icon(
+          Icons.mail,
+          color: Colors.white,
+        ),
+      ),
+      MenuItem(
+        title: 'Power',
+        image: Icon(
+          Icons.power,
+          color: Colors.white,
+        ),
+      ),
+      MenuItem(
+        title: 'Setting',
+        image: Icon(
+          Icons.settings,
+          color: Colors.white,
+        ),
+      ),
+      MenuItem(
+        title: 'PopupMenu',
+        image: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+      )
+    ];*/
+  PopupMenu menu = PopupMenu(
+    // backgroundColor: Colors.teal,
+    // lineColor: Colors.white,
+    // maxColumn: 2,
+    items: items,
+    onClickMenu: _iconItemClicked,
+    stateChanged: _iconStateChanged,
+    onDismiss: _iconMenuDismissed,
+  );
+  menu.show(widgetKey: menuKey);
+}
+
+void _iconStateChanged(bool isShow) {
+  print('menu is ${isShow ? 'showing' : 'closed'}');
+}
+
+void _iconMenuDismissed() {
+  print('Menu is dismissed');
+}
+
+void _iconItemClicked(MenuItemProvider item) {
+  _setNewIconForTask(
+    currentSelectedTask,
+    currentSelectedIndex,
+    item.menuTitle,
+  );
+}
+
+void _setNewIconForTask(DailyTask task, int index, String iconString) {
+  setState(() {
+    task.iconString = iconString;
+  });
+  DataStore.updateSingleTask(task, index);
+  print('Updated Icon to ${task.iconString}');
+}
--- lib/task_screen.dart
+++ lib/task_screen.dart
@@ -6,6 +6,7 @@ import 'package:popup_menu/popup_menu.dart';
 import 'package:daily_task_app/daily_task.dart';
 import 'package:daily_task_app/data_store.dart';
 import 'package:daily_task_app/intervals.dart';
+import 'package:daily_task_app/popup_icon_menu.dart';
 
 class TaskScreen extends StatefulWidget {
   const TaskScreen({Key key, this.appBarTitle}) : super(key: key);
@@ -17,6 +18,8 @@ class TaskScreen extends StatefulWidget {
     return _TaskScreenState();
   }
 }
+
+// TODO(MZ): Create CellState class that contains a task and the cellstate (open, done)
 
 class _TaskScreenState extends State<TaskScreen> {
   List<DailyTask> _dailyTasks = <DailyTask>[];
@@ -238,7 +241,7 @@ class _TaskScreenState extends State<TaskScreen> {
           OutlineButton(
             key: keyOpenIconMenu,
             onPressed: () {
-              _openIconMenu();
+              openIconMenu(iconStrings, keyOpenIconMenu);
             },
             child: currentTask.getIcon(),
           ),
@@ -359,101 +362,6 @@ class _TaskScreenState extends State<TaskScreen> {
     //setState(() {
     //  task.iconString = iconString;
     //});
-    DataStore.updateSingleTask(task, index);
-    print('Updated Icon to ${task.iconString}');
-  }
-
-  // TODO(MZ): Create CellState class that contains a task and the cellstate (open, done)
-  void _openIconMenu() {
-    List<MenuItem> items = iconStrings.map(
-      (String currentIconString) {
-        return MenuItem(
-          title: currentIconString,
-          image: Icon(
-            MdiIcons.fromString(currentIconString),
-            color: Colors.white,
-          ),
-        );
-      },
-    ).toList();
-
-    /*List<MenuItem> items = <MenuItem>[
-      MenuItem(
-        title: 'Copy',
-        image: Icon(
-          Icons.home,
-          color: Colors.white,
-        ),
-      ),
-      MenuItem(
-        title: 'Home',
-        textStyle: TextStyle(fontSize: 10.0, color: Colors.tealAccent),
-        image: Icon(
-          Icons.home,
-          color: Colors.white,
-        ),
-      ),
-      MenuItem(
-        title: 'Mail',
-        image: Icon(
-          Icons.mail,
-          color: Colors.white,
-        ),
-      ),
-      MenuItem(
-        title: 'Power',
-        image: Icon(
-          Icons.power,
-          color: Colors.white,
-        ),
-      ),
-      MenuItem(
-        title: 'Setting',
-        image: Icon(
-          Icons.settings,
-          color: Colors.white,
-        ),
-      ),
-      MenuItem(
-        title: 'PopupMenu',
-        image: Icon(
-          Icons.menu,
-          color: Colors.white,
-        ),
-      )
-    ];*/
-    PopupMenu menu = PopupMenu(
-      // backgroundColor: Colors.teal,
-      // lineColor: Colors.white,
-      // maxColumn: 2,
-      items: items,
-      onClickMenu: _iconItemClicked,
-      stateChanged: _iconStateChanged,
-      onDismiss: _iconMenuDismissed,
-    );
-    menu.show(widgetKey: keyOpenIconMenu);
-  }
-
-  void _iconStateChanged(bool isShow) {
-    print('menu is ${isShow ? 'showing' : 'closed'}');
-  }
-
-  void _iconMenuDismissed() {
-    print('Menu is dismissed');
-  }
-
-  void _iconItemClicked(MenuItemProvider item) {
-    _setNewIconForTask(
-      currentSelectedTask,
-      currentSelectedIndex,
-      item.menuTitle,
-    );
-  }
-
-  void _setNewIconForTask(DailyTask task, int index, String iconString) {
-    setState(() {
-      task.iconString = iconString;
-    });
     DataStore.updateSingleTask(task, index);
     print('Updated Icon to ${task.iconString}');
   }
